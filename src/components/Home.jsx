0d8e2e10813b2fb66fa7777681ad0cbb0ea642d4
simd22 <113197504+simd22@users.noreply.github.com>
2025-06-05 20:25:45 -0700

    browser tab & wording changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,61 @@
-import React from 'react'
-import Projects from './Projects';
-import { Link as ScrollLink } from 'react-scroll';
-import bitmoji from '../assets/bitmoji.png'
-import Typewriter from 'typewriter-effect';
+import React from "react";
+import Projects from "./Projects";
+import { Link as ScrollLink } from "react-scroll";
+import bitmoji from "../assets/bitmoji.png";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen pt-[60px] bg-[#F3E5FA]'> 
-        {/* Container */}
-        <div className='max-w-[1100px] mx-auto px-8 flex flex-col justify-center h-full md:flex-row md:items-center'>
-            
-            {/* TEXT */}
-            <div className='max-w-[650px] md:flex flex-col'>
-                <h2 className='text-1xl sm:text-4xl font-bold text-pink-600'>Hi, I'm Simeon! {' '}
-                    <div className="text-1xl sm:text-4xl text-[#0019C1]">
-                        <Typewriter 
-                            className="z-0"
-                            options={{
-                            strings: ['Learner', 'Leader', 'Explorer'],
-                            autoStart: true,
-                            loop: true,
-                            cursor: '',
-                            }}
-                        />
-                    </div>
-                </h2>
-                <p className='text-1xl sm:text-3xl py-4 '>I’m a student-athlete at Brown University studying Computer Engineering. I'm passionate about innovating reliable software products in tech that solve user needs.</p>
-
-                <div className='flex'>
-                    <ScrollLink activeClass="active" to="projects" smooth={true} duration={500}>
-                        <button className='bg-pink-600 text-[#F3E5FA] group border-2 px-6 py-3 my-2 flex items-center rounded-full hover:bg-[#E4F8E6] hover:border-[#E4F8E6] hover:text-[#4CAF50]'>
-                            See Projects
-                        </button>
-                    </ScrollLink>
-                </div>
+    <div name="home" className="w-full h-screen pt-[60px] bg-[#F3E5FA]">
+      {/* Container */}
+      <div className="max-w-[1100px] mx-auto px-8 flex flex-col justify-center h-full md:flex-row md:items-center">
+        {/* TEXT */}
+        <div className="max-w-[650px] md:flex flex-col">
+          <h2 className="text-1xl sm:text-4xl font-bold text-pink-600">
+            Hi, I'm Simeon!{" "}
+            <div className="text-1xl sm:text-4xl text-[#0019C1]">
+              <Typewriter
+                className="z-0"
+                options={{
+                  strings: ["Learner", "Leader", "Explorer"],
+                  autoStart: true,
+                  loop: true,
+                  cursor: "",
+                }}
+              />
             </div>
-
-            {/* IMAGE */}
-            <div className='md:w-1/2 mt-4 md:mt-0 text-center'>
-                <img src={bitmoji} alt='Headshot' className='mx-auto w-60 h-60 md:w-80 md:h-80' />
-            </div>
+          </h2>
+          <p className="text-1xl sm:text-3xl py-4 ">
+            I recently graduated from Brown University as a student-athlete and
+            am returning for a Fifth Year Master's in Computer Science. I'm
+            passionate about solving big picture challenges to drive excellence.
+          </p>
+          <div className="flex">
+            <ScrollLink
+              activeClass="active"
+              to="projects"
+              smooth={true}
+              duration={500}
+            >
+              <button className="bg-pink-600 text-[#F3E5FA] group border-2 px-6 py-3 my-2 flex items-center rounded-full hover:bg-[#E4F8E6] hover:border-[#E4F8E6] hover:text-[#4CAF50]">
+                See Projects
+              </button>
+            </ScrollLink>
+          </div>
         </div>
-        <Projects />
-    </div>
-  )
-}
 
-export default Home
+        {/* IMAGE */}
+        <div className="md:w-1/2 mt-4 md:mt-0 text-center">
+          <img
+            src={bitmoji}
+            alt="Headshot"
+            className="mx-auto w-60 h-60 md:w-80 md:h-80"
+          />
+        </div>
+      </div>
+      <Projects />
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,127 +7,163 @@ import portmock from '../assets/portmock.jpg'
 
 const Projects = () => {
   return (
-    <div name='projects' className='w-full md:h-screen text-black bg-[#F3E5FA]'>
-        <div className='max-w-[1100px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-1xl sm:text-4xl font-bold inline border-b-4 text-pink-600 border-pink-600'>Projects</p>
-                <p className='text-1xl sm:text-2xl py-6'>Check out some ways that I've learned new concepts!</p>
-            </div>
-            
-            {/* CONTAINER */}
-            <div className='grid sm:grid-cols-1 md:grid-cols-3 gap-4 '>
-                {/* SPOTIFY GRID ITEM */}
-                <div className='flex flex-col space-y-2'>
-                    {/* PICTURE */}
-                    <RouterLink to="/spotify">
-                        <div style={{ backgroundImage: `url(${spotify})`}} 
-                        className='hover:scale-105 shadow-lg shadow-[#B36BCC] group container rounded-md flex justify-center text-center items-center mx-auto content-div hover:scale-1.02 transform transition-transform duration-200 ease-in-out '>
-                            {/* Hover Effects */}
-                            <div className='opacity-0 group-hover:opacity-100 flex flex-col items-center'>
-                                <span className='text-m sm:text-xl font-bold text-white tracking-wider'>
-                                    Behind Spotify
-                                </span>
-                                <div className='text-m sm:text-xl max-w-[200px] text-[#00A5E0]'>
-                                    Incorporating the Spotify API for user authentication and user info
-                                </div>
-                            </div>
-                        </div>
-                    
-                    </RouterLink>
-
-                    {/* CAPTION */}
-                    <div className='pt-4 flex flex-col items-center space-y-2'>
-                        <div className='pr-8'>
-                            <p className='text-m sm:text-2xl text-pink-600 font-bold'>Behind Spotify</p>
-                        </div>
-                        {/* React, HTML, CSS, JavaScript, Firebase */}
-                        <div className='flex justify-center space-x-2'>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">React</p>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">Spotify API</p>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">Firebase</p>
-                        </div>
-                    </div>
-                </div>
-
-                {/* PORTFOLIO GRID ITEM */}
-                <div className='flex flex-col space-y-2'>
-
-                    <RouterLink to="/portfolio">
-                        {/* PICTURE */}
-                        <div style={{ backgroundImage: `url(${portmock})`}} 
-                        className='hover:scale-105 shadow-lg shadow-[#B36BCC] group container rounded-md flex justify-center text-center items-center mx-auto content-div hover:scale-1.02 transform transition-transform duration-200 ease-in-out'>
-        
-                            <div className='opacity-0 group-hover:opacity-100 flex flex-col items-center'>
-                                <span className='text-m sm:text-xl font-bold text-white tracking-wider'>
-                                    Personal Portfolio
-                                </span>
-                                <div className='text-m sm:text-xl max-w-[200px] text-[#00A5E0]'>
-                                    Getting creative with React and Tailwind CSS
-                                </div>
-                            </div>
-                        </div>
-                    </RouterLink>
-
-                    {/* Caption */}
-                    <div className='pt-4 flex flex-col items-center space-y-2'>
-                        <div className='pr-8'>
-                            <p className='text-m sm:text-2xl text-pink-600 font-bold'>Personal Portfolio</p>
-                        </div>
-                        {/* React, HTML, CSS, JavaScript, Firebase */}
-                        <div className='flex justify-center space-x-2'>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">React</p>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">Firebase</p>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">Tailwind CSS</p>
-                        </div>
-                    </div>
-                </div>
-
-                {/* SHORTENER GRID ITEM */}
-                <div className='flex flex-col space-y-2'>
-                    <RouterLink to="/shortener">
-                        {/* PICTURE */}
-                        <div style={{ backgroundImage: `url(${shortmock})`}} 
-                        className='hover:scale-105 shadow-lg shadow-[#B36BCC] group container rounded-md flex justify-center text-center items-center mx-auto content-div hover:scale-1.02 transform transition-transform duration-200 ease-in-out'>
-                            
-                            {/* Hover Effects */}
-                            <div className='opacity-0 group-hover:opacity-100 flex flex-col items-center'>
-                                <span className='text-m sm:text-2xl font-bold text-white tracking-wider'>
-                                    Shorten Pls
-                                </span>
-                                <div className='text-m sm:text-xl max-w-[200px] text-[#00A5E0]'>
-                                    Using MongoDB database for storing and shortening links
-                                </div>
-                            </div>
-                        </div>
-                    </RouterLink>
-
-                    {/* Caption */}
-                    <div className='pt-4 flex flex-col items-center space-y-2'>
-                        <div className='pr-8'>
-                            <p className='text-m sm:text-2xl text-pink-600 font-bold'>Shorten Pls</p>
-                        </div>
-                        {/* React, HTML, CSS, JavaScript, Firebase */}
-                        <div className='flex justify-center space-x-2'>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">HTML</p>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">CSS</p>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">JS</p>
-                            <p className="bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s cursor-pointer hover:bg-blue-300 hover:text-blue-900 transition duration-300">MongoDB</p>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-            <div className="flex flex-col items-center justify-center pt-16">
-            <ScrollLink activeClass="active" to="home" smooth={true} duration={500}>  
-                <button className='bg-pink-600 text-[#F3E5FA] group border-2 px-6 py-3 my-2 flex items-center rounded-full hover:bg-[#E4F8E6] hover:border-[#E4F8E6] hover:text-[#4CAF50]'>
-                    Back to Top
-                </button>
-            </ScrollLink>
-             </div>
+    <div name="projects" className="w-full md:h-screen text-black bg-[#F3E5FA]">
+      <div className="max-w-[1100px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-1xl sm:text-4xl font-bold inline border-b-4 text-pink-600 border-pink-600">
+            Projects
+          </p>
+          <p className="text-1xl sm:text-2xl py-6">
+            Check out some ways that I've learned new skills!
+          </p>
         </div>
+
+        {/* CONTAINER */}
+        <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-4 ">
+          {/* SPOTIFY GRID ITEM */}
+          <div className="flex flex-col space-y-2">
+            {/* PICTURE */}
+            <RouterLink to="/spotify">
+              <div
+                style={{ backgroundImage: `url(${spotify})` }}
+                className="hover:scale-105 shadow-lg shadow-[#B36BCC] group container rounded-md flex justify-center text-center items-center mx-auto content-div hover:scale-1.02 transform transition-transform duration-200 ease-in-out "
+              >
+                {/* Hover Effects */}
+                <div className="opacity-0 group-hover:opacity-100 flex flex-col items-center">
+                  <span className="text-m sm:text-xl font-bold text-white tracking-wider">
+                    Behind Spotify
+                  </span>
+                  <div className="text-m sm:text-xl max-w-[200px] text-[#00A5E0]">
+                    Incorporating the Spotify API for user authentication and
+                    user info
+                  </div>
+                </div>
+              </div>
+            </RouterLink>
+
+            {/* CAPTION */}
+            <div className="pt-4 flex flex-col items-center space-y-2">
+              <div className="pr-8">
+                <p className="text-m sm:text-2xl text-pink-600 font-bold">
+                  Behind Spotify
+                </p>
+              </div>
+              {/* React, HTML, CSS, JavaScript, Firebase */}
+              <div className="flex justify-center space-x-2">
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  React
+                </p>
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  Spotify API
+                </p>
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  Firebase
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* PORTFOLIO GRID ITEM */}
+          <div className="flex flex-col space-y-2">
+            <RouterLink to="/portfolio">
+              {/* PICTURE */}
+              <div
+                style={{ backgroundImage: `url(${portmock})` }}
+                className="hover:scale-105 shadow-lg shadow-[#B36BCC] group container rounded-md flex justify-center text-center items-center mx-auto content-div hover:scale-1.02 transform transition-transform duration-200 ease-in-out"
+              >
+                <div className="opacity-0 group-hover:opacity-100 flex flex-col items-center">
+                  <span className="text-m sm:text-xl font-bold text-white tracking-wider">
+                    Personal Portfolio
+                  </span>
+                  <div className="text-m sm:text-xl max-w-[200px] text-[#00A5E0]">
+                    Getting creative with React and Tailwind CSS
+                  </div>
+                </div>
+              </div>
+            </RouterLink>
+
+            {/* Caption */}
+            <div className="pt-4 flex flex-col items-center space-y-2">
+              <div className="pr-8">
+                <p className="text-m sm:text-2xl text-pink-600 font-bold">
+                  Personal Portfolio
+                </p>
+              </div>
+              {/* React, HTML, CSS, JavaScript, Firebase */}
+              <div className="flex justify-center space-x-2">
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  React
+                </p>
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  Firebase
+                </p>
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  Tailwind CSS
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* SHORTENER GRID ITEM */}
+          <div className="flex flex-col space-y-2">
+            <RouterLink to="/shortener">
+              {/* PICTURE */}
+              <div
+                style={{ backgroundImage: `url(${shortmock})` }}
+                className="hover:scale-105 shadow-lg shadow-[#B36BCC] group container rounded-md flex justify-center text-center items-center mx-auto content-div hover:scale-1.02 transform transition-transform duration-200 ease-in-out"
+              >
+                {/* Hover Effects */}
+                <div className="opacity-0 group-hover:opacity-100 flex flex-col items-center">
+                  <span className="text-m sm:text-2xl font-bold text-white tracking-wider">
+                    Shorten Pls
+                  </span>
+                  <div className="text-m sm:text-xl max-w-[200px] text-[#00A5E0]">
+                    Using MongoDB database for storing and shortening links
+                  </div>
+                </div>
+              </div>
+            </RouterLink>
+
+            {/* Caption */}
+            <div className="pt-4 flex flex-col items-center space-y-2">
+              <div className="pr-8">
+                <p className="text-m sm:text-2xl text-pink-600 font-bold">
+                  Shorten Pls
+                </p>
+              </div>
+              {/* React, HTML, CSS, JavaScript, Firebase */}
+              <div className="flex justify-center space-x-2">
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  HTML
+                </p>
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  CSS
+                </p>
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  JS
+                </p>
+                <p className="cursor-default bg-[#FCF9DC] text-[#DF9ECD] bg-opacity-70 text-opacity-80 px-2 py-1 rounded-full text-xs sm:text-s hover:bg-blue-300 hover:text-blue-900 transition duration-300">
+                  MongoDB
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center pt-16">
+          <ScrollLink
+            activeClass="active"
+            to="home"
+            smooth={true}
+            duration={500}
+          >
+            <button className="bg-pink-600 text-[#F3E5FA] group border-2 px-6 py-3 my-2 flex items-center rounded-full hover:bg-[#E4F8E6] hover:border-[#E4F8E6] hover:text-[#4CAF50]">
+              Back to Top
+            </button>
+          </ScrollLink>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Projects
